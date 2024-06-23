c1bf6193c99479436e1425b7a4f24dd4cb939481
xixi019 <xixi019@users.noreply.github.com>
2024-06-23 21:08:40 +0000

    deploy: 70a073b737f46563189648dede1715bc87312000

---
+++ _app/immutable/chunks/paths.83be78f2.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_3iw8ii)==null?void 0:s.base)??"/leaderboard";var a;const i=((a=globalThis.__sveltekit_3iw8ii)==null?void 0:a.assets)??e;export{i as a,e as b};
